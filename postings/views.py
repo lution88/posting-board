83f39e46818ce8d021b9068b531bdd2a856e7f5a
lution88 <lution88@gmail.com>
2022-09-06 11:08:59 +0900

    feat: Posting 모델 생성

---
+++ postings/views.py
@@ -0,0 +1,9 @@
+# from django.shortcuts import render
+# from rest_framework import status
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+# from postings.models import Posting
+# from postings.serializers import PostingSerializer
+
