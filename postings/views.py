7ed2b7ea9d87694ab501510a16edfff07b9fa857
lution88 <lution88@gmail.com>
2022-09-06 11:53:15 +0900

    feat: PostsAPI 조회, 생성, 수정, 삭제 단순 기능 구현(비밀번호 적용안됨)

--- postings/views.py
+++ postings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import status
+from rest_framework.generics import get_object_or_404
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -20,3 +21,22 @@ class PostsAPI(APIView):
             post_serializer.save()
             return Response(post_serializer.data, status=status.HTTP_200_OK)
         return Response(post_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, posting_id):
+        post = Posting.objects.get(id=posting_id)
+        post_serializer = PostingSerializer(post, data=request.data, partial=True)
+        if post_serializer.is_valid():
+            post_serializer.save()
+            return Response(post_serializer.data, status=status.HTTP_200_OK)
+        return Response(post_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        return Response({"message": "delete success"})
+
+
+class PostAPI(APIView):
+    def get(self, request, post_id):
+        post = get_object_or_404(Posting, id=post_id)
+        post_serializer = PostingSerializer(post)
+        return Response(post_serializer.data, status=status.HTTP_200_OK)
+
