3231f1e8ef57cff1327a6cca8de117d8e37a01e8
lution88 <lution88@gmail.com>
2022-09-06 13:51:07 +0900

    fix: posting시 제목과 본문 이모지 포함하도록 수정

--- postings/serializers.py
+++ postings/serializers.py
@@ -6,13 +6,13 @@ from .models import Posting
 class PostingSerializer(ModelSerializer):
     class Meta:
         model = Posting
-        fields = ['id', 'title', 'content', 'password']
+        fields = ["id", "title", "content", "password"]
         extra_kwargs = {
-            'password': {'write_only': True},
+            "password": {"write_only": True},
         }
 
     def create(self, validated_data):
         post = Posting(**validated_data)
+
         post.save()
         return post
-
--- postings/views.py
+++ postings/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+import os
+import bcrypt
+
 from rest_framework import status
 from rest_framework.generics import get_object_or_404
 
@@ -17,6 +19,11 @@ class PostsAPI(APIView):
 
     def post(self, request):
         post_serializer = PostingSerializer(data=request.data)
+        password = request.data["password"]
+
+        hashed_password = bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+        decoded_password = hashed_password.decode("utf-8")
+
         if post_serializer.is_valid():
             post_serializer.save()
             return Response(post_serializer.data, status=status.HTTP_200_OK)
@@ -30,13 +37,20 @@ class PostsAPI(APIView):
             return Response(post_serializer.data, status=status.HTTP_200_OK)
         return Response(post_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request):
-        return Response({"message": "delete success"})
+    def delete(self, request, posting_id):
+        post = Posting.objects.get(id=posting_id)
+        input_password = request.data["password"]
+        print(input_password)
+
+        if input_password == post.password:
+            post.delete()
+            return Response({"message": "삭제 완료"})
+        return Response({"message": "비밀번호를 다시 입력해 주세요."})
 
 
 class PostAPI(APIView):
     def get(self, request, post_id):
         post = get_object_or_404(Posting, id=post_id)
         post_serializer = PostingSerializer(post)
-        return Response(post_serializer.data, status=status.HTTP_200_OK)
 
+        return Response(post_serializer.data, status=status.HTTP_200_OK)
