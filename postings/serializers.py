f6ac3c9c5dbe0a92dd97f93e298f184e7542c9b2
lution88 <lution88@gmail.com>
2022-09-06 19:55:52 +0900

    fix: PostingSerializer extra_kwargs:'password' 옵션 추가

--- postings/serializers.py
+++ postings/serializers.py
@@ -4,11 +4,23 @@ from .models import Posting
 
 
 class PostingSerializer(ModelSerializer):
+    ''' 게시글 시리얼라이저 ModelSerializer 사용 '''
     class Meta:
+        # 사용 모델
         model = Posting
+        # 사용 필드들
         fields = ["id", "title", "content", "password", "dt_created"]
+        # 기타 옵션
         extra_kwargs = {
-            "password": {"write_only": True},
+            "password": {
+                # 쓰기 전용
+                "write_only": True,
+                # 에러 메세지 설정.
+                "error_message": {
+                    "required": "비밀번호를 입력해주세요.",
+                    "invalid": "비밀번호가 틀렸습니다. 다시 입력해주세요."
+                },
+            },
         }
 
     def create(self, validated_data):
@@ -16,3 +28,4 @@ class PostingSerializer(ModelSerializer):
         password = post.password
         post.save()
         return post
+
