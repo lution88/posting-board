aa493dbf88d8d908c06dc76efca07dc0dcfda7bf
lution88 <lution88@gmail.com>
2022-09-06 19:53:54 +0900

    fix: PostsAPI 전체 API 와 PostAPI 단건 API 분리

--- postings/views.py
+++ postings/views.py
@@ -12,6 +12,7 @@ from postings.serializers import PostingSerializer
 
 
 class PostsAPI(APIView):
+    ''' POSTS 전체 API '''
     def get(self, request):
         posts = Posting.objects.all().order_by("-dt_created")
         posts_serializer = PostingSerializer(posts, many=True)
@@ -28,6 +29,15 @@ class PostsAPI(APIView):
             post_serializer.save()
             return Response(post_serializer.data, status=status.HTTP_200_OK)
         return Response(post_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PostAPI(APIView):
+    ''' POST 단건 API '''
+    def get(self, request, post_id):
+        post = get_object_or_404(Posting, id=post_id)
+        post_serializer = PostingSerializer(post)
+
+        return Response(post_serializer.data, status=status.HTTP_200_OK)
 
     def put(self, request, posting_id):
         post = Posting.objects.get(id=posting_id)
@@ -46,11 +56,3 @@ class PostsAPI(APIView):
             post.delete()
             return Response({"message": "삭제 완료"})
         return Response({"message": "비밀번호를 다시 입력해 주세요."})
-
-
-class PostAPI(APIView):
-    def get(self, request, post_id):
-        post = get_object_or_404(Posting, id=post_id)
-        post_serializer = PostingSerializer(post)
-
-        return Response(post_serializer.data, status=status.HTTP_200_OK)
